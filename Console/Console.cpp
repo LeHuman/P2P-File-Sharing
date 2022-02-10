d2fe778710a71d4b46b0a97c2053ac2ee4d4fdb3
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-02-09 20:35:39 -0600

    comments

--- Console/Console.cpp
+++ Console/Console.cpp
@@ -30,14 +30,14 @@ namespace Console {
 		string func = tokens[0];
 		transform(func.begin(), func.end(), func.begin(), ::tolower);
 
-		if (func == RPC::k_Deregister) { // Shouldn't be called directly?
+		if (func == RPC::k_Deregister) { // Shouldn't be called directly
 			bool deregistered = indexer.deregister((Index::entryHash_t)tokens[1]);
 			if (deregistered) {
 				Log.i(ID, "Deregisted hash: %s", tokens[1].data());
 			} else {
 				Log.e(ID, "Unable to deregister hash: %s", tokens[1].data());
 			}
-		} else if (func == RPC::k_Register) { // Shouldn't be called directly?
+		} else if (func == RPC::k_Register) { // Shouldn't be called directly
 			bool registered = indexer.registry(tokens[1], (Index::entryHash_t)tokens[2]);
 			if (registered) {
 				Log.i(ID, "Registered hash: %s", tokens[1].data());
@@ -56,7 +56,7 @@ namespace Console {
 			}
 		} else if (func == RPC::k_Ping) {
 			Log.i(ID, "Server reponse time: %dms", indexer.ping());
-		} else if (func == RPC::k_Request) { // Should call async to start downloading
+		} else if (func == RPC::k_Request) { // Should call async to start downloading from sources / a source
 			Log.i(ID, "Searching server peer index for: %s", tokens[1].data());
 			for (Index::Peer::searchEntry &item : indexer.request((Index::entryHash_t)tokens[1])) {
 				std::cout << item.str().data() << std::endl;
