b0bea36c635717759e3ecf8aaad56ccaf7021e51
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-02-19 23:15:10 -0600

    allow defaults

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -7,10 +7,10 @@ int main(int argc, char *argv[]) {
 		TCLAP::CmdLine cmd("Create a P2P Client", ' ');
 
 		TCLAP::ValueArg<uint32_t> idArg("i", "identity", "Unique ID identifying this client", true, 0, "int", cmd);
-		TCLAP::ValueArg<std::string> sipArg("s", "serverIP", "The indexing server IP address this client should connect to", true, "localhost", "ip address", cmd);
-		TCLAP::ValueArg<uint16_t> spArg("e", "serverPort", "The indexing server Port this client should use", true, 55555, "int", cmd);
-		TCLAP::ValueArg<uint16_t> cpArg("c", "clientPort", "The client Port other peers should connect to", true, 55555, "int", cmd);
-		TCLAP::ValueArg<std::string> fldrArg("f", "downloadFolder", "The local folder files should be uploaded and downloaded to", true, "localhost", "directory", cmd);
+		TCLAP::ValueArg<std::string> sipArg("s", "serverIP", "The indexing server IP address this client should connect to", false, "localhost", "ip address", cmd);
+		TCLAP::ValueArg<uint16_t> spArg("e", "serverPort", "The indexing server Port this client should use", false, 55555, "int", cmd);
+		TCLAP::ValueArg<uint16_t> cpArg("c", "clientPort", "The client Port other peers should connect to", false, 44000, "int", cmd);
+		TCLAP::ValueArg<std::string> fldrArg("f", "downloadFolder", "The local folder files should be uploaded and downloaded to", true, "", "directory", cmd);
 
 		cmd.parse(argc, argv);
 
