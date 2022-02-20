dc68d25c9467e97f28cc10457591c04c2222c898
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-02-19 20:40:15 -0600

    default

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -4,11 +4,13 @@
 int main(int argc, char *argv[]) {
 	try {
 		TCLAP::CmdLine cmd("Create a P2P Server", ' ');
-		TCLAP::ValueArg<uint16_t> portArg("p", "port", "The port this server should listen to", true, 55555, "int", cmd);
+		TCLAP::ValueArg<uint16_t> portArg("p", "port", "The port this server should listen to", false, 55555, "int", cmd);
 		cmd.parse(argc, argv);
 
 		Index::Indexer s(portArg.getValue()); // Create Indexing server given port from the arguments
 		s.start(); // Start the Indexing server
+
+		// Log.enable(false);
 
 		while (true) {
 			std::this_thread::sleep_for(std::chrono::seconds(1));
