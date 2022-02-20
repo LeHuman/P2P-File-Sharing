336a2ac85568da27b99644eeb4a45d1d337867c6
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-02-20 00:05:22 -0600

    default port with id

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
 		TCLAP::ValueArg<uint32_t> idArg("i", "identity", "Unique ID identifying this client", true, 0, "int", cmd);
 		TCLAP::ValueArg<std::string> sipArg("s", "serverIP", "The indexing server IP address this client should connect to", false, "localhost", "ip address", cmd);
 		TCLAP::ValueArg<uint16_t> spArg("e", "serverPort", "The indexing server Port this client should use", false, 55555, "int", cmd);
-		TCLAP::ValueArg<uint16_t> cpArg("c", "clientPort", "The client Port other peers should connect to", false, 44000, "int", cmd);
+		TCLAP::ValueArg<uint16_t> cpArg("c", "clientPort", "The client Port other peers should connect to", false, 44000 + idArg.getValue(), "int", cmd);
 		TCLAP::ValueArg<std::string> fldrArg("f", "downloadFolder", "The local folder files should be uploaded and downloaded to", true, "", "directory", cmd);
 
 		cmd.parse(argc, argv);
