5ffb0dcc169822b4b4733479e79def53501fe100
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-03-27 20:47:29 -0500

    pass additional folders

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -28,21 +28,40 @@ int main(int argc, char *argv[]) {
 
 		TCLAP::ValueArg<uint32_t> idArg("i", "identity", "Unique ID identifying this client", false, 0, "int", cmd);
 		TCLAP::ValueArg<std::string> confArg("c", "configFile", "The config file to use", false, "test_config.json", "filePath", cmd);
-		TCLAP::ValueArg<std::string> fldrArg("f", "downloadFolder", "The local folder files should be uploaded and downloaded to", false, "", "directory", cmd);
+		TCLAP::ValueArg<std::string> upFldrArg("u", "uploadFolder", "The local folder files should be uploaded from", false, "", "directory", cmd);
+		TCLAP::ValueArg<std::string> dnFldrArg("d", "downloadFolder", "The local folder files should be downloaded to", false, "", "directory", cmd);
 		TCLAP::SwitchArg all2all("a", "all2all", "enable all2all mode", cmd);
 
 		cmd.parse(argc, argv);
 
-		Config::config_t config = Config::getConfig(idArg.getValue(), confArg.getValue(), all2all.getValue());
+		Config::config_t config;
 
-		string folder = fldrArg.getValue();
-
-		if (folder == "") {
-			folder = "watchFolder" + std::to_string(idArg.getValue());
+		try {
+			config = Config::getConfig(idArg.getValue(), confArg.getValue(), all2all.getValue());
+		} catch (const std::exception &e) {
+			Log.e("Client", "Error reading config file: %s", e.what());
+			exit(1);
 		}
 
-		if (!std::filesystem::exists(folder)) {
-			std::filesystem::create_directories(folder);
+		string originFolder = upFldrArg.getValue();
+
+		if (originFolder == "") {
+			originFolder = "originFolder" + std::to_string(idArg.getValue());
+		}
+
+		if (!std::filesystem::exists(originFolder)) {
+			std::filesystem::create_directories(originFolder);
+			Log.w("Client", "Watch Folder auto created");
+		}
+
+		string remoteFolder = dnFldrArg.getValue();
+
+		if (remoteFolder == "") {
+			remoteFolder = "remoteFolder" + std::to_string(idArg.getValue());
+		}
+
+		if (!std::filesystem::exists(remoteFolder)) {
+			std::filesystem::create_directories(remoteFolder);
 			Log.w("Client", "Watch Folder auto created");
 		}
 
@@ -50,10 +69,10 @@ int main(int argc, char *argv[]) {
 		Index::Indexer *s;
 
 		// Create a Peer object
-		Peer c(idArg.getValue(), config.port + 1000, config.server.ip, config.server.port, folder); //1000 is added to port to differentiate client to server when on super peers
+		Peer c(idArg.getValue(), config.port + 1000, config.server.ip, config.server.port, originFolder, remoteFolder, config.pushing, config.pulling); //1000 is added to port to differentiate client to server when on super peers
 
 		if (config.isSuper) { // Create server if this is a super peer
-			s = new Index::Indexer(config.server.port, config.totalSupers, config.all2all);
+			s = new Index::Indexer(config.server.port, config.totalSupers, config.pushing, config.pulling, config.all2all);
 			for (Index::conn_t conn : config.neighbors) {
 				if (conn != config.server) { // Ensure we don't reference ourselves
 					s->addNeighboor(conn);
